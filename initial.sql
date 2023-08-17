3f16fec529fa19be133467e95c9f2b26ea516d40
ivymuchacha <ivy83718@gmail.com>
2023-08-17 11:08:58 +0800

    Update initial sql

--- initial.sql
+++ initial.sql
@@ -1,7 +1,7 @@
-INSERT INTO `scooter` (`name`, `license_number`, `price`, `state`) VALUES ('scooter one', 'AAA-001', 50, 1);
-INSERT INTO `scooter` (`name`, `license_number`, `price`, `state`) VALUES ('scooter two', 'AAA-002', 80, 1);
 INSERT INTO `wemo`.`rentStatus` (`id`, `name`) VALUES (1, '租借中');
 INSERT INTO `wemo`.`rentStatus` (`id`, `name`) VALUES (2, '已結束');
 INSERT INTO `wemo`.`scooterState` (`id`, `name`) VALUES (1, '正常');
 INSERT INTO `wemo`.`scooterState` (`id`, `name`) VALUES (2, '維修中');
 INSERT INTO `wemo`.`scooterState` (`id`, `name`) VALUES (3, '已報廢');
+INSERT INTO `scooter` (`name`, `license_number`, `price`, `stateId`) VALUES ('scooter one', 'AAA-001', 50, 1);
+INSERT INTO `scooter` (`name`, `license_number`, `price`, `stateId`) VALUES ('scooter two', 'AAA-002', 80, 1);
